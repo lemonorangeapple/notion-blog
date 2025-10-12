541309c3f3e6225b7c4c7e7d50d46a9f32d7e30a
June_qwq <107529938+lemonorangeapple@users.noreply.github.com>
2025-10-12 12:29:13 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -2,8 +2,7 @@ import styles from './styles.module.css'
 
 export function GitHubShareButton() {
   return (
-    <div style="display: none;">
-      { styles.githubCorner }
+    <div Id="{ styles.githubCorner }">
     </div>
   )
 }
